dd57739391658aba56415b0e83f7a25fef7f925f
solomon18 <solomonjc0218@gmail.com>
2018-08-20 05:37:26 +0900

    Add wrap up on redux-react simple concept

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'Javascript: The Good Parts' },
-        { title: 'Harry Potter' },
-        { title: 'Lord of the Rings' },
-        { title: 'Introduction to Algorithms' }
+        { title: 'Javascript: The Good Parts', pages: 101 },
+        { title: 'Harry Potter', pages: 852 },
+        { title: 'Lord of the Rings', pages: 951 },
+        { title: 'Introduction to Algorithms', pages: 1000 }
     ]
 }
